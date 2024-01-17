1324ebc72484740dd8831c991e28f0e008e03692
Diogo <ggdiogogg@gmail.com>
2024-01-16 21:19:18 -0300

    até modulo 17

--- http-exercicios/src/App.vue
+++ http-exercicios/src/App.vue
@@ -1,24 +1,42 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-alert show dismissible v-for="mensagem in mensagens" :key="mensagem.texto"
+		:variant="mensagem.tipo">{{ mensagem.texto }}</b-alert>
 		<b-card>
 			<b-form-group label="Nome:">
-				<b-form-input type="texto" size="lg" v-molde="usuario.nome"
+				<b-form-input type="texto" size="lg" v-model="usuario.nome"
 				placeholder="Informe o Nome"></b-form-input>
 			</b-form-group>
 		</b-card>
 		<b-card>
 			<b-form-group label="E-mail:">
-				<b-form-input type="email" size="lg" v-molde="usuario.email"
+				<b-form-input type="email" size="lg" v-model="usuario.email"
 				placeholder="Informe o Email"></b-form-input>
 			</b-form-group>
 		</b-card>
 		<hr>
 		<b-button @click="salvar" size="lg" variant="primary">Salvar</b-button>
+		<b-button @click="obterUsuarios" size="lg" variant="success" class="ml-2">Obter Usuarios</b-button>
+		<hr>
+		<b-list-group>
+			<b-list-group-item v-for="(usuario, id) in usuarios" :key="id">
+				<strong>Nome: </strong> {{ usuario.nome }} <br>
+				<strong>E-mail: </strong> {{ usuario.email }} <br>
+				<strong>ID: </strong> {{ id }} <br>
+				<b-button variant="warning" size=lg
+				@click="carregar(id)">Alterar</b-button>
+				<b-button variant="danger" size="lg" class="ml-2"
+				@click="excluir(id)">Excluir</b-button>
+			</b-list-group-item>
+		</b-list-group>
 	</div>
 </template>
 
 <script>
+
+import axios from 'axios'
+
 export default {
 	// created() {
 	// 	this.$http.post('usuarios.json', {
@@ -29,6 +47,9 @@ export default {
 
 	data(){
 		return {
+			mensagens: [],
+			id: null,
+			usuarios: [],
 			usuario: {
 				nome:'',
 				email: '',
@@ -36,9 +57,58 @@ export default {
 		}
 	},
 	methods: {
+		limpar() {
+			this.usuario.nome = ''
+			this.usuario.email = ''
+			this.id = null
+			this.mensagens = []
+		},
+
+		carregar(id) {
+			this.id = id
+			this.usuario = { ...this.usuarios[id] }
+		},
+
+		//post
 		salvar() {
-			console.log(this.usuario)
-		}
+			const metodo = this.id ? 'patch' : 'post'
+			const finalUrl = this.id ? `/${this.id}.json` : '.json'
+			this.$http[metodo](`/usuarios${finalUrl}`, this.usuario).then(() => {
+				this.limpar()
+				this.mensagens.push({
+					texto: 'Operação realizada com sucesso',
+					tipo: 'success'
+				})
+			})
+		},
+
+		excluir(id) {
+			this.$http.delete(`/usuarios/${id}.json`)
+			.then( () => this.limpar())
+			.catch (err => {
+				this.mensagens.push({
+					texto: 'Problema para exlcuir!',
+					tipo: 'danger'
+				})
+			})
+		},
+
+ 		//get, pode se usar this.$http.get também / importanto axios localmente pode usar simplesmente "axios" mas adicione a url base por completo 
+		obterUsuarios(){
+			this.$http('usuarios.json')
+			.then(res => {
+				this.usuarios = res.data
+				console.log(res.data)
+			})
+
+			// axios('https://curso-vue-54d17-default-rtdb.firebaseio.com/usuarios.json')
+			// .then(res => {
+			// 	this.usuarios = res.data
+			// 	console.log(this.usuarios)
+			// })
+
+		},
+
 	},
 }
 </script>
--- http-exercicios/src/plugins/axios.js
+++ http-exercicios/src/plugins/axios.js
@@ -2,11 +2,30 @@ import Vue from 'vue'
 import axios from 'axios'
 
 //axios.defaults.baseURL = 'https://curso-vue-54d17-default-rtdb.firebaseio.com/'
+// axios.defaults.headers.common['Authorization'] = 'abc123'
+// axios.defaults.headers.get['Accepts'] = 'application/json'
 
 Vue.use({
     install(Vue) {
         Vue.prototype.$http = axios.create({
-            baseURL: 'https://curso-vue-54d17-default-rtdb.firebaseio.com/'
+            baseURL: 'https://curso-vue-54d17-default-rtdb.firebaseio.com/',
+            headers: {
+                    "Authorization" : "abc123"
+            }
         })
+
+        Vue.prototype.$http.interceptors.request.use(config => {
+            console.log(config.method)
+            return config
+        }, error => Promise.reject(error))
+
+        Vue.prototype.$http.interceptors.response.use(res => {
+            // const array = []
+            // for(let chave in res.data) {
+            //     array.push({ id: chave, ...res.data[chave] }) // "..." significa 'todos os atributos com essa chave especifica' na verdade é 'nome: res.data[chave].nome, email: res.data[chave].email'
+            // }
+            // res.data = array
+            return res
+        }, error => Promise.reject(error))
     }
 })
